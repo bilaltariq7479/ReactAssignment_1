f59a36d941a4072111e0b863cb15810fcb92667a
bilaltariq7479 <blackhat7479@gmail.com>
2024-05-08 12:00:15 +0500

    adding header file

---
+++ src/layout/header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Logo from "../assets/logo.png";
+
+const Header = () => {
+  return (
+    <div className="page_width">
+      <header>
+        <div className="logo">
+            <img src={Logo} alt="" />
+        </div>
+        <div className="nav_links">
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Courses</li>
+            <li>Contact us</li>
+            <li>Faq's</li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
