e24abaafcddfb96554a9ef55d7c136d601227963
bilaltariq7479 <blackhat7479@gmail.com>
2024-05-14 10:39:28 +0500

    updating files

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 import Layout from './layout';
 import HomePage from './pages/homepage';
 import Aboutus from './pages/aboutus';
+import {Routes, Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App"> 
       <Layout>
-        <HomePage />
-        <Aboutus />
+      <Routes>
+          <Route path="/" exact element={<HomePage />} />
+          <Route path="/about_us" exact element={<Aboutus />} />
+          {/* <Route path="/services" exact element={<ContactUs />} />
+          <Route path="/sign_up" exact element={<SignUp />} /> */}
+        </Routes>
+        {/* <HomePage />
+        <Aboutus /> */}
       </Layout>
     </div>
   );
--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import Logo from "../assets/logo.png";
-
+import {Link} from 'react-router-dom'
 const Header = () => {
   return (
     <div className="page_width">
       <header>
         <div className="logo">
-            <img src={Logo} alt="" />
-            <input type="text" placeholder="" />
+          <img src={Logo} alt="" />
+          <input type="text" placeholder="" />
         </div>
         <div className="nav_links">
           <ul>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Courses</li>
-            <li>Contact us</li>
-            <li>Faq's</li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about_us">About Us</Link>
+            </li>
           </ul>
         </div>
       </header>
